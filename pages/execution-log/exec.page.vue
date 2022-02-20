d3d3961088f92b54e66089acd7b5a511f73aaa98
SyedHassan Asheghi <Asheghi.sh@gmail.com>
2022-02-20 19:55:43 +0330

    minor change

--- pages/execution-log/exec.page.vue
+++ pages/execution-log/exec.page.vue
@@ -3,10 +3,22 @@
     <div class="text-xl mb-2">Execution History of {{ name }}</div>
     <div class="mb-4">{{ data.date }}</div>
     <div class="exec" v-for="(val,key) in data.exec">
-      <div class="text-lg font-bold">{{key}}</div>
-      <div class="stdout">{{val.stdout}}</div>
-      <div class="stderr text-red-500" v-text="val.stderr"/>
-      <div class="err text-red-700" v-text="val.err"/>
+      <div class="text-lg font-bold">{{ key }}</div>
+      <div class="stdout">
+        <pre>
+          <code>
+            {{ val.stdout }}
+          </code>
+        </pre>
+      </div>
+      <div class="stderr text-red-500">
+        <pre><code>
+          {{ val.stderr }}
+        </code></pre>
+      </div>
+      <div class="err text-red-700">
+        <pre><code>{{ val.err }}</code></pre>
+      </div>
     </div>
   </div>
 </template>
@@ -16,18 +28,18 @@ import {usePageContext} from "../../renderer/usePageContext";
 import {ax} from "../../lib/plugins/axios";
 
 export default {
-  setup(){
-    const {routeParams:{name,id}} = usePageContext()
-    return {name,id};
+  setup() {
+    const {routeParams: {name, id}} = usePageContext()
+    return {name, id};
   },
   mounted() {
     if (!this.data) {
       this.fetchData();
     }
   },
-  methods:{
+  methods: {
     async fetchData() {
-      const {data, status} = await ax.get('github/history/' + this.name+'/'+this.id);
+      const {data, status} = await ax.get('github/history/' + this.name + '/' + this.id);
       this.data = data;
     }
   },
